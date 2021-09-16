ec4c085fb2d63d78ee0de64a46f852da2beb0589
Andrew Nichols <tandrewnichols@gmail.com>
2021-09-16 17:39:21 -0400

    Add vim skeletons to repo

---
+++ .vim/skeletons/jsx/spec.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Subject from './`!v expand('%:t:r:r')`';
+import { shallow } from 'enzyme';
+
+describe('`!v join(split(expand('%:.:r:r'), '/')[1:], '/')`', () => {
+  ${0}
+});
