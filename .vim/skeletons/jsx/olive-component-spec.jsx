7be758a66a1640cae92fabf83dd47941d3e357bd
Andrew Nichols <tandrewnichols@gmail.com>
2021-09-28 10:23:41 -0400

    Add olive skeletons

---
+++ .vim/skeletons/jsx/olive-component-spec.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { act } from 'react-dom/test-utils';
+
+import Subject from '${1}';
+
+describe('${2}', () => {
+  ${0}
+});
---
+++ .vim/skeletons/jsx/olive-component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ${1} = ({ ${2} }) => {
+  return (
+    ${0}
+  );
+};
+
+$1.propTypes = {
+
+};
+
+export default $1;
